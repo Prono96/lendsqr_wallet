67a7cfd3b539c6e202bb6b0e9328dce35891f423
Prono96 <promiseprince01@gmail.com>
2024-07-07 09:40:18 +0100

    still modifying the knex config

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,6 @@ const envPath = process.env.NODE_ENV == 'development' ? '.env' : '.env.productio
 config({ path: envPath });
 
 
-console.log('Database:', process.env.DB_NAME);
-console.log('Using .env file:', envPath);
-console.log('User:', process.env.DB_USER);
-console.log('Password:', process.env.DB_PASSWORD);
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
